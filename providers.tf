75992cf8009c8e81161eee2b8ec70fd169568652
Leider000 <leidergiovannyparada@gmail.com>
2023-06-12 11:06:34 -0500

    Registro de archivos de configuracion

---
+++ providers.tf
@@ -0,0 +1,27 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">=4.36.0, <4.47.0, !=4.43.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "3.4.3"
+    }
+  }
+  required_version = ">1.3.0"
+}
+
+provider "aws" {
+  region = "us-east-1"
+  access_key = var.access_key
+  secret_key = var.secret_key
+  default_tags {
+    tags = var.tags
+  }
+}
+
+
+
+
+
